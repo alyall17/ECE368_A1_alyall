8777d8a791df713893fd0662f277b1cf0860fb76
Ava Lyall <avalyall@Avas-MacBook-Air-4.local>
2024-08-29 19:05:43 -0400

    Initial main.c write completed

--- main.c
+++ main.c
@@ -27,3 +27,63 @@ void bubbleSort(int combos[][4], int numCombos){
         }
     }
 }
+
+// Determines and outputs all coin combinations for an integer input
+void printCombos(int cents){
+    // LOCAL CONSTANTS
+    const int QUARTER = 25;
+    const int DIME = 10;
+    const int NICKEL = 5;
+    const int PENNY = 1;
+
+    // ARRAYS AND VARIABLES
+    int combos[10000][4]; // Coin combos array
+    int numCombos = 0; // Total number of combinations for a specific input
+    int q; // Quarters lcv (loop control variable)
+    int d; // Dimes lcv
+    int n; // Nickels lcv
+    int p; // Pennies lcv
+    int i; // Printing combos lcv
+
+    // EXECUTABLE STATEMENTS
+    // Brute force approach to generating all combos
+    for(q = 0; q <= cents / QUARTER; q++){
+        for(d = 0; d <= (cents - q * QUARTER) / DIME; d++){
+            for(n = 0; (cents - q * QUARTER - d * DIME) / NICKEL; n++){
+                p = cents - q * QUARTER - d * DIME - n * NICKEL;
+                if(p >= 0){
+                    combos[numCombos][0] = q;
+                    combos[numCombos][1] = d;
+                    combos[numCombos][2] = n;
+                    combos[numCombos][3] = p;
+                    numCombos++;
+                }
+            }
+        }
+    }
+
+    // Sort the combinations using a bubble sort algorithm
+    bubbleSort(combos, numCombos);
+
+    // Output all combinations
+    for(i = 0; i < numCombos; i++){
+        printf("%d quarter(s), %d dime(s), %d nickel(s), %d pennie(s)\n",
+                combos[i][0], combos[i][1], combos[i][2], combos[i][3]);
+    }
+
+    return(0);
+}
+
+int main(){
+    // VARIABLES
+    int cents; // Inputted cent value
+
+    // EXECUTABLE STATEMENTS
+    printf("Enter the number of cents: ");
+    scanf("%d", &cents);
+
+    // Print all valid combinations
+    printCombos(cents);
+
+    return(0);
+}
